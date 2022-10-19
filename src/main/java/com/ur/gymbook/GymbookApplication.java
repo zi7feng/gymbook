98457fa471e03ba4d3af52c27b95a836125bd67d
fzq <zfeng16@ur.rochester.edu>
2022-10-19 13:06:29 -0400

    init

---
+++ src/main/java/com/ur/gymbook/GymbookApplication.java
@@ -0,0 +1,13 @@
+package com.ur.gymbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymbookApplication.class, args);
+    }
+
+}
