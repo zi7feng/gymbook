572e87ac1a15eed59d1c01f3907c9bbcb1d83169
fzq <zfeng16@ur.rochester.edu>
2022-10-28 17:06:55 -0400

    test rust

---
+++ src/main/java/com/ur/gymbook/controller/TestC.java
@@ -0,0 +1,12 @@
+package com.ur.gymbook.controller;
+
+import com.ur.gymbook.model.test;
+
+public class TestC {
+
+
+    public static void main(String[] args) {
+        test t = new test();
+        System.out.println(t);
+    }
+}
---
+++ src/main/java/com/ur/gymbook/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.ur.gymbook.controller;
+
+import com.ur.gymbook.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("USER")
+public class UserController {
+
+//    @Autowired
+//    private UserService userService;
+}
